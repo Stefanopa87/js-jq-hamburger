55a1bccf6097f71fb91e9ca3ef782d53d6e02505
Stefano Patti <mastepa@hotmail.com>
2021-03-18 15:59:35 +0100

    altre prove

--- main.js
+++ main.js
@@ -1,7 +1,36 @@
-function init(){
+
+// con TOGGLECLASS metto e levo ogni volta che clicco
+//  CON FUNZIONE ***ANONIMA***
+function init1(){
   $('.fas').click(function(){
     $('.hamburger-menu').toggleClass('active');
   });
 }
 
-init();
+// init1();
+
+// ---------------------------------
+
+// con TOGGLECLASS metto e levo ogni volta che clicco
+//  CON FUNZIONE ***DICHIARTA***
+function premi(){
+  $('.hamburger-menu').toggleClass('active');
+}
+
+function init2(){
+  $('.fas').click(premi);
+}
+
+init2();
+
+// ----------------------------------
+
+//  con ADDCLASS aggiungo una regola di css ad un elemento
+//  che però rimane "fissa"
+function init3(){
+  $('.hamburger-menu').addClass('active');
+}
+
+// init3();
+
+// ----------------------------------
